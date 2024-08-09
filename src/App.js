cfe86e9515738c792693ca428efd9d7a2b7ef519
Darshana Dinkar Biradar <darshanabi@cybage.com>
2024-08-09 20:29:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-
-import './App.css';
+import Body from "./Components/Body";
 
 function App() {
-  return (
-    <div className="text-3xl font-bold text-green-800">
-      Namaste Everyone, let's build NetflixGPT
-    </div>
-  );
+  return <Body/>;
+  
 }
 
 export default App;
